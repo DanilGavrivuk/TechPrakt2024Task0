e9ea8ec75eec861aea5a34da0fe44847ff044b22
Vitalik-Sadok <vsadolinskij@gmail.com>
2024-02-25 18:50:41 +0200

    first commit

---
+++ Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+            Console.WriteLine($"Сумма: {a + b}");
+        }
+    }
+}
